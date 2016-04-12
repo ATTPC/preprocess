f5d02f42cbd30d83b0d6c08ba5079833d5947461
Michelle Kuchera <kucheram@nscl.msu.edu>
2016-04-12 13:11:24 -0400

    added pad plan rotation and removed some calculations from the events loop

--- clean/pyclean_main.py
+++ clean/pyclean_main.py
@@ -2,8 +2,10 @@
 import pyclean 
 import argparse
 import pytpc
+#import math
 import numpy as np
 import h5py
+from pytpc.constants import pi, degrees
 
 def main():
     parser = argparse.ArgumentParser(description='A script to clean data and write results to an HDF5 file')
@@ -16,8 +18,10 @@ def main():
     #efile = pytpc.EventFile(args.input,'r')
     #with pytpc.HDFDataFile(args.output, 'a') as hfile:
      #   gp = hfile.fp.require_group(hfile.get_group_name)
-    
+    v_drift = pytpc.simulation.drift_velocity_vector(-5.2,9000,1.75,0.10472)
+    tmat = pytpc.utilities.tilt_matrix(-0.10472)
     inFile =  pytpc.HDFDataFile(args.input, 'r')
+    pad_plane = pytpc.generate_pad_plane(rotation_angle=-108*degrees)
     print(inFile)
     with h5py.File(args.output, 'a') as outFile:
         
@@ -45,13 +49,17 @@ def main():
         for i in range(evts_to_process):
             evt = inFile[start+i]
             print(start+i)
-            raw_xyz = evt.xyzs(peaks_only=True, return_pads=True)
-            v_drift = pytpc.simulation.drift_velocity_vector(-5.2,9000,1.75,0.10472)
+           # pad_plane = pytpc.generate_pad_plane(rotation_angle=-108*degrees)
+        #print(len(gp))
+            raw_xyz = evt.xyzs(pads=pad_plane, peaks_only=True, return_pads=True,cg_times=True)
+            #v_drift = pytpc.simulation.drift_velocity_vector(-5.2,9000,1.75,0.10472)
+            
             uvw = pytpc.evtdata.calibrate(raw_xyz,v_drift,12.5)
-            tmat = pytpc.utilities.tilt_matrix(-0.10472)
+            #tmat = pytpc.utilities.tilt_matrix(-0.10472)
             uvw = np.dot(tmat,uvw[:,:3].T).T
             clean_uvw, center_uv = pyclean.clean(uvw)
-            clean_xyz = np.column_stack((raw_xyz,clean_uvw[:,-1]))
+            nearest_neighbors = pyclean.nearest_neighbor_count(uvw,40) 
+            clean_xyz = np.column_stack((raw_xyz,nearest_neighbors,clean_uvw[:,-1]))
             gp = outFile.require_group('clean')
             deset = gp.create_dataset('{:d}'.format(evt.evt_id), data=clean_xyz, compression='gzip', shuffle=True)
             center_uvw = np.hstack((center_uv,0))
@@ -59,42 +67,8 @@ def main():
             center_xyz = np.dot(tmat.T,center_uvw)
             #print(center_xyz)
             deset.attrs['center'] = center_xyz[:2]
-
-#evt = gp[str(i)]
-            #xyz = evt[:,:]
-            #print(xyz)
-        # work with the file
-        #print(hfile.get_group_name)
-        #evt = hfile[128]  # for example
-        #print(evt)
-        #xyz = evt[:,0:4]
  
     return 0
-
-    #gp = []
-"""
-    all_evts = set(efile.evtids)
-
-    if len(gp) > 0:
-        finished_evts = set(int(k) for k in gp.keys() if k.isdigit())
-        evts_to_process = all_evts - finished_evts
-        if len(evts_to_process) > 0:
-            print('Already processed {} events. Continuing from where we left off.'.format(len(finished_evts)))
-        else:
-            print('All events have already been processed.')
-            sys.exit(0)
-    else:
-        evts_to_process = all_evts
-        #for i in smart_progress_bar(evts_to_process):
-        for i in evts_to_process:
-            evt = efile.get_by_event_id(i)
-            raw_xyz = evt.xyzs(peaks_only=True)
-            v_drift = pytpc.simulation.drift_velocity_vector(-5.2,9000,1.75,0.10472)
-            uvw = pytpc.evtdata.calibrate(raw_xyz,v_drift,12.5)
-            tmat = pytpc.utilities.tilt_matrix(-0.10472)
-            uvw = np.dot(tmat,uvw[:,:3].T).T
-            pyclean.clean(uvw)
-     """       
 
 
 if __name__ == '__main__':

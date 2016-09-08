f86dba42bdcc79de163c65e33abe53720cef1727
Joshua Bradt <bradt@nscl.msu.edu>
2016-09-08 11:23:46 -0400

    Refactored a bit more. Moved all code to 1 file. Updated pytpc.

--- clean/pyclean.py
+++ clean/pyclean.py
@@ -19,6 +19,21 @@ E.g. for 46Ar experiment, 8mm was a good cutoff for this paramemter, giving quit
 
 import numpy as np
 import math
+from math import sin, cos
+import argparse
+import pytpc
+import sys
+import h5py
+from pytpc.constants import degrees
+import pytpc.simulation
+
+import logging
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+formatter = logging.Formatter('[%(levelname)s:%(name)s] %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
 
 
 def nearest_neighbor_count(data, radius):
@@ -268,3 +283,91 @@ def clean(xyz):
     center = [a, b]
 
     return clean_xyz, center
+
+
+def event_iterator(num_input_evts, num_output_evts):
+    num_events_remaining = num_input_evts - num_output_evts
+    if num_events_remaining == 0:
+        logger.warning('All events have already been processed.')
+        raise StopIteration()
+    elif num_output_evts > 0:
+        logger.info('Already processed %d events. Continuing from where we left off.', num_output_evts)
+
+    first_evt = num_output_evts
+
+    for i in range(first_evt, num_input_evts):
+        if i % 100 == 0:
+            logger.info('At event %d / %d', i, num_input_evts)
+        yield i
+    else:
+        raise StopIteration()
+
+
+class EventCleaner(object):
+    def __init__(self):
+        self.v_drift = pytpc.simulation.drift_velocity_vector(-5.2, 9000, 1.75, 0.10472)
+        self.tmat = pytpc.utilities.tilt_matrix(-0.10472)
+        self.un_tmat = pytpc.utilities.tilt_matrix(0.10472)
+        self.un_rotate_ang = 108 * degrees
+        self.pad_plane = pytpc.generate_pad_plane(rotation_angle=-108 * degrees)
+        self.rot = np.array([[cos(self.un_rotate_ang), -sin(self.un_rotate_ang)],
+                             [sin(self.un_rotate_ang), cos(self.un_rotate_ang)]])
+
+    def process_event(self, evt):
+        raw_xyz = evt.xyzs(pads=self.pad_plane, peaks_only=True, return_pads=True,
+                           cg_times=True, baseline_correction=True)
+        uvw = pytpc.evtdata.calibrate(raw_xyz, self.v_drift, 12.5)
+        uvw = np.dot(self.tmat, uvw[:, :3].T).T
+        clean_uvw, center_uv = clean(uvw)
+        nearest_neighbors = nearest_neighbor_count(uvw, 40)
+        clean_xyz = np.column_stack((raw_xyz, nearest_neighbors, clean_uvw[:, -1]))
+
+        cc = [center_uv[0], center_uv[1], 0]
+        cc = np.dot(self.un_tmat, cc)
+        cc = np.dot(self.rot, cc[:2].T).T
+
+        return clean_xyz, cc
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='A script to clean data and write results to an HDF5 file')
+    parser.add_argument('input', help='The input evt file')
+    parser.add_argument('output', help='The output HDF5 file')
+    args = parser.parse_args()
+
+    inFile = pytpc.HDFDataFile(args.input, 'r')
+
+    cleaner = EventCleaner()
+
+    with h5py.File(args.output, 'a') as outFile:
+        gp = outFile.require_group('clean')
+        logger.info('Finding length of input file...')
+        num_input_evts = len(inFile)
+        logger.info('Input file contains %d events', num_input_evts)
+
+        num_output_evts = len(gp)
+
+        for evt_index in event_iterator(num_input_evts, num_output_evts):
+            try:
+                evt = inFile[evt_index]
+                clean_xyz, cc = cleaner.process_event(evt)
+                deset = gp.create_dataset('{:d}'.format(evt.evt_id), data=clean_xyz, compression='gzip', shuffle=True)
+                deset.attrs['center'] = cc
+            except(KeyError) as err:
+                print(err)
+                continue
+
+
+if __name__ == '__main__':
+    import signal
+
+    def handle_signal(signum, stack_frame):
+        logger.critical('Received signal %d. Quitting.', signum)
+        sys.stdout.flush()
+        sys.exit(1)
+
+    signal.signal(signal.SIGTERM, handle_signal)
+    signal.signal(signal.SIGQUIT, handle_signal)
+
+    main()
